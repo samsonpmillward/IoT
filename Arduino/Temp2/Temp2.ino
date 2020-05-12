fef9c140d86cd57bacc8b8c3ee7842bc63effa50
samson millward <up820264@myport.ac.uk>
2020-05-12 14:51:14 +0100

    Added Extra Button
    outputs hello currently

--- Arduino/Temp2/Temp2.ino
+++ Arduino/Temp2/Temp2.ino
@@ -5,6 +5,7 @@
 dht DHT;
 U8GLIB_SH1106_128X64 u8g(4, 5, 6, 7);  // SW SPI Com: SCK = 4, MOSI = 5, CS = 6, A0 = 7 (new blue HalTec OLED)
 
+int buttonPin = 2;
 
 #define DHT11_PIN 3
 
@@ -13,25 +14,26 @@ void u8g_prepare(void) {
   u8g.setFontRefHeightExtendedText();
   u8g.setDefaultForegroundColor();
   u8g.setFontPosTop();
-  u8g.setPrintPos(15,15);
+  u8g.setPrintPos(3,15);
 }
 
 
 
 void draw(void) {
   u8g_prepare();
+  u8g.print("Temperature = ");
   u8g.print(DHT.temperature);
   
 }
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   
   // flip screen, if required
   u8g.setRot180();
 
-  //int buttonPin = 2;
-  //pinMode(13, OUTPUT);           
+  
+  pinMode(buttonPin, INPUT_PULLUP);           
   //digitalWrite(13, HIGH);  
 }
 
@@ -40,6 +42,11 @@ void loop()
 {
   u8g.firstPage();
   int chk = DHT.read11(DHT11_PIN);
+  int buttonValue = digitalRead(buttonPin);
+  if (buttonValue == LOW) {
+    Serial.println("hello");
+    delay(1000);
+  }
   if (DHT.temperature != -999) {
     
       //int chk = DHT.read11(DHT11_PIN);
@@ -47,13 +54,14 @@ void loop()
       Serial.println(DHT.temperature);
       Serial.print("Humidity = ");
       Serial.println(DHT.humidity);
+      
       do {
         draw();
       }
       while ( u8g.nextPage() );
       
       //u8g.print(DHT.temperature);
-      delay(100);
+      //delay(1000);
       
   }
 }
